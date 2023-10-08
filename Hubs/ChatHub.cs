820c13fbe3dbda0565c030500c247deb66070c34
Eric-Gonzalez-Portfolio <35201416+Eric-Gonzalez-Portfolio@users.noreply.github.com>
2023-10-08 04:04:38 +0000

    Chat rooms added

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,9 +1,29 @@
 using Microsoft.AspNetCore.SignalR;
 
+// public class ChatHub : Hub
+// {
+//     public async Task SendMessage(string user, string message)
+//     {
+//         await Clients.All.SendAsync("ReceiveMessage", user, message);
+//     }
+// }
+
 public class ChatHub : Hub
 {
-    public async Task SendMessage(string user, string message)
+    public async Task JoinRoom(string roomName)
     {
-        await Clients.All.SendAsync("ReceiveMessage", user, message);
+        await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+        await Clients.Group(roomName).SendAsync("ReceiveMessage", "Server", $"{Context.ConnectionId} has joined the room.");
+    }
+
+    public async Task LeaveRoom(string roomName)
+    {
+        await Clients.Group(roomName).SendAsync("ReceiveMessage", "Server", $"{Context.ConnectionId} has left the room.");
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+    }
+
+    public async Task SendMessageToRoom(string roomName, string message)
+    {
+        await Clients.Group(roomName).SendAsync("ReceiveMessage", Context.ConnectionId, message);
     }
 }
