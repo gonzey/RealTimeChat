ab9b3978401c8c2cb6229f9992f80c6fcf3a0f1b
davidbodey <75290134+davidbodey@users.noreply.github.com>
2023-10-09 10:42:47 +0000

    file sharing through torrent almost works

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,11 +1,34 @@
+using System.Collections.Concurrent;
 using Microsoft.AspNetCore.SignalR;
 
 public class ChatHub : Hub
 {
+    private static readonly ConcurrentDictionary<string, string> ConnectionToRoomMap = new ConcurrentDictionary<string, string>();
+    private static readonly ConcurrentDictionary<string, List<string>> RoomToUsersMap = new ConcurrentDictionary<string, List<string>>();
+
     public async Task JoinRoom(string roomName, string user)
     {
+        Console.WriteLine($"Joining room {roomName} for user {user}");
         await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+        await Clients.Caller.SendAsync("JoinedRoom", roomName);
         await Clients.Group(roomName).SendAsync("ReceiveMessage", Context.ConnectionId, "Server", $"{user} has joined the room.");
+        ConnectionToRoomMap[Context.ConnectionId] = roomName;
+        RoomToUsersMap.GetOrAdd(roomName, new List<string>()).Add(user);
+        await Clients.Group(roomName).SendAsync("UpdateUserList", RoomToUsersMap[roomName]);
+
+    }
+    public override async Task OnDisconnectedAsync(Exception exception)
+    {
+        if (ConnectionToRoomMap.TryRemove(Context.ConnectionId, out string roomName))
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+            // Assuming you also have a way to map the connection ID back to the user name
+            // For now, let's just say "A user" has left
+            RoomToUsersMap[roomName].Remove("A user"); // Replace "A user" with actual username if possible
+            await Clients.Group(roomName).SendAsync("UpdateUserList", RoomToUsersMap[roomName]);
+            await Clients.Group(roomName).SendAsync("ReceiveMessage", Context.ConnectionId, "Server", "A user has left the room.");
+        }
+        await base.OnDisconnectedAsync(exception);
     }
 
     public async Task LeaveRoom(string roomName, string user)
@@ -19,9 +42,10 @@ public class ChatHub : Hub
         await Clients.Group(roomName).SendAsync("ReceiveMessage", Context.ConnectionId, user, message);
     }
 
-    public async Task SendMultimediaToRoom(string roomName, string user, string base64String, string fileType)
+    public async Task SendTorrentDetailsToRoom(string roomName, string user, string fileName, long fileSize, string infoHash)
     {
-        await Clients.Group(roomName).SendAsync("ReceiveMultimedia", user, base64String, fileType);
+        Console.WriteLine($"Announcing new torrent in room {roomName} from user {user} with infoHash {infoHash}");
+        await Clients.Group(roomName).SendAsync("ReceiveTorrentDetails", user, fileName, fileSize, infoHash);
     }
 
 }
